c5f57dfca3754f2139076079d76b6fb43391f08f
7DevianT7 <bojan8522@gmail.com>
2025-07-28 13:58:14 +0200

    Initial commit

---
+++ Zadaca28072025/src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero-content">
+        <h1>IT Solutions that transform your business</h1>
+        <h2>TROPIC TECH SOLUTIONS</h2>
+        <button className="learn-more-btn">Learn More</button>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
